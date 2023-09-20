c75b1ead6cfab64dd2b41f24e2b67034f85c1893
programmer-weeb <forprogramming8@gmail.com>
2023-09-20 13:59:50 +0300

    passpor use LocalStrategy

--- app.js
+++ app.js
@@ -1,6 +1,25 @@
 const express = require('express')
+const passport = require('passport')
+const LocalStrategy = require('passport-local').Strategy
 const app = express()
 const port = 3000
+const AuthorModel = require('./models/AuthorModel')
+const bcrypt = require('bcryptjs')
+
+app.use(express.urlencoded({ extended: false }))
+
+passport.use(
+    new LocalStrategy(async (username, password, done) => {
+        const author = await AuthorModel.findOne({ username }).exec()
+        const doesPasswordMatch = await bcrypt.compare(password, author.password)
+        if (doesPasswordMatch) {
+            return done(null, author)
+        } else {
+            return done(null, false, { message: 'invalid password' })
+        }
+    })
+)
+
 
 app.get('/', (req, res) => res.send('Hello World!'))
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
